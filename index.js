74c64ace238eb9cee48499ee6ba4116acc3ab2e5
Donald Hruska <donaldhruska@gmail.com>
2015-04-28 20:40:51 -0500

    Streamline logic

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ if (NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 
-app.listen(PORT, () => console.log(`LISTENING @ http://127.0.0.1:${PORT}`))
+app.listen(PORT, ()=> console.log(`LISTENING @ http://127.0.0.1:${PORT}`))
 
-app.get('*', sendHeaders, (req, res) => {
+app.get('*', setFileMeta, sendHeaders, (req, res) => {
 	if (res.body) {
 		res.json(res.body)
 		return
@@ -28,20 +28,23 @@ app.get('*', sendHeaders, (req, res) => {
 	fs.createReadStream(req.filePath).pipe(res)
 })
 
-app.head('*', sendHeaders, (req, res) => res.end())
+app.head('*', setFileMeta, sendHeaders, (req, res) => res.end())
+
+function setFileMeta(req, res, next) {
+	req.filePath = path.resolve(path.join(ROOT_DIR, req.url))
+	if (filePath.indexOf(ROOT_DIR) !== 0) {
+		res.send(400, 'Invalid path')
+		return
+	}
+	fs.promise.stat(filePath)
+		.then(stat => req.stat = stat)
+		.nodeify(next)
+}
 
 function sendHeaders(req, res, next) {
-	nodeify(async () => {
-		let filePath = path.resolve(path.join(ROOT_DIR, req.url))
-		req.filePath = filePath
-		if (filePath.indexOf(ROOT_DIR) !== 0) {
-			res.send(400, 'Invalid path')
-			return
-		}
-
-		let stat = await fs.promise.stat(filePath)
-		if (stat.isDirectory()) {
-			let files = await fs.promise.readdir(filePath)
+	nodeify(async ()=> {
+		if (req.stat.isDirectory()) {
+			let files = await fs.promise.readdir(req.filePath)
 			res.body = JSON.stringify(files)
 			res.setHeader('Content-Length', res.body.length)
 			res.setHeader('Content-Type', 'application/json')
@@ -49,8 +52,8 @@ function sendHeaders(req, res, next) {
 		}
 
 		res.setHeader('Content-Length', stat.size)
-		let contentType = mime.contentType(path.extname(filePath))
+		let contentType = mime.contentType(path.extname(req.filePath))
 		res.setHeader('Content-Type', contentType)
 
-	}(), next)
+	}(), next) // Use nodeify to call next on success or failure
 }
